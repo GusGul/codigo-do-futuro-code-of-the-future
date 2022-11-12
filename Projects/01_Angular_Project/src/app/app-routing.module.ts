edd43420e9bb5cf06e307f9d449f021a5e91e0d5
Gustavo Salgado Lima <gustavosalgadolima@gmail.com>
2022-11-12 09:50:12 -0300

    organizacao

---
+++ Projects/01_Angular_Project/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
