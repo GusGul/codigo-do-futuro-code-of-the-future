5b50358465b8fcc0e4e5b43307cd76cb51b88c7d
Gustavo Salgado Lima <gustavosalgadolima@gmail.com>
2022-11-12 00:31:28 -0300

    data-binding e componentes

--- FrontEnd/01_Angular_Project/src/app/app.module.ts
+++ FrontEnd/01_Angular_Project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
